ac59b8757b39fba04517a4c08f51a3093c6998d6
nyuusen <pppike.0131@gmail.com>
2025-12-06 23:15:03 +0900

    pipe client

--- pipe/client/main.go
+++ pipe/client/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Filepath string `json:"filepath"`
+}
 
 func main() {
 	fmt.Println("exec client.go")
+
+	// ファイルOpen処理(FD割り当て)
+	f, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal("open file error:", err)
+	}
+	// リソースリークを防止するためCloseする
+	defer f.Close()
+
+	// ファイル内容を読み取る
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal("file read error:", err)
+	}
+
+	// ファイル内容を構造体に詰める
+	config := &Config{}
+	err = json.Unmarshal(b, config)
+	if err != nil {
+		log.Fatal("json unmarshal error:", err)
+	}
+	fp := config.Filepath
+	log.Printf("filepath: %s\n", fp)
+
+	// 名前付きファイルを取得
+	pipe, err := os.OpenFile(fp, os.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal("open named pipe file error:", err)
+	}
+	defer pipe.Close()
+
+	scanner := bufio.NewScanner(pipe)
+	for {
+		if scanner.Scan() {
+			data := scanner.Text()
+			fmt.Println("receiving data from pipe:", data)
+		} else {
+			if scnErr := scanner.Err(); scnErr != nil {
+				fmt.Println("pipe was closed:", scnErr)
+				break
+			}
+		}
+	}
+
 }
