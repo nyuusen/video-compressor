ebfa5cdd901fa9e18fd7cfec0a899702017d233f
nyuusen <pppike.0131@gmail.com>
2025-12-07 23:06:28 +0900

    socket server

---
+++ socket/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	fmt.Println("exec socket server")
+
+	// リスナーを作成
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+	defer listener.Close()
+	fmt.Println("server start")
+
+	// 接続待ち
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("accept error:", err)
+		}
+		go func(c net.Conn) {
+			defer c.Close()
+			// データ受信
+			buf := make([]byte, 1024)
+			// ユーザー空間バッファにコピー
+			n, err := c.Read(buf)
+			if err != nil {
+				log.Fatal("read error:", err)
+			}
+			fmt.Printf("receive data %s (size: %d)", string(buf), n)
+		}(conn)
+	}
+}
